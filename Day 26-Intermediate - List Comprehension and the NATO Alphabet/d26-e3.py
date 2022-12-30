02824fd190e677c5da2795105833ac0f67b621db
Alan Biju Palayil <77706228+AlanP13@users.noreply.github.com>
2022-12-30 12:47:34 -0500

    Commit Day 2

---
+++ Day 26-Intermediate - List Comprehension and the NATO Alphabet/d26-e3.py
@@ -0,0 +1,11 @@
+with open("d26-e3-file1.txt") as file1:
+    list1 = file1.readlines()
+    
+with open("d26-e3-file2.txt") as file2:
+    list2 = file2.readlines()
+    
+result = [int(num) for num in list1 if num in list2]
+
+# Write your code above 👆
+
+print(result)
