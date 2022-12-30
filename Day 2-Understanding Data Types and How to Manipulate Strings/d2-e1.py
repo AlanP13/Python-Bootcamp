911c62286bd9faadaabe93e6057f8d247f567746
Alan Biju Palayil <77706228+AlanP13@users.noreply.github.com>
2022-12-29 22:49:29 -0500

    Commit Day #1

---
+++ Day 2-Understanding Data Types and How to Manipulate Strings/d2-e1.py
@@ -0,0 +1,11 @@
+# 🚨 Don't change the code below 👇
+number = input("Type a two digit number: ")
+# 🚨 Don't change the code above 👆
+
+####################################
+#Write your code below this line 👇
+fdigit = int(number[0])
+sdigit = int(number[1])
+
+total = fdigit + sdigit
+print(total)
